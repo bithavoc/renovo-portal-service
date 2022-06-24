6628468742daf1b23fa861d997e2fbfa90ae6b11
bithavoc <im@bithavoc.io>
2022-06-24 14:09:53 +0000

    backupAgentJob

--- resolvers/Protections.ts
+++ resolvers/Protections.ts
@@ -196,9 +196,144 @@ export class VeeamProtectionBackupServerJobMeta {
 }
 
 @ObjectType()
+export class VeeamProtectionBackupAgentJobMeta {
+  @Field({ nullable: true })
+  instanceUid?: string;
+
+  @Field({ nullable: true, description: 'UID assigned to a Veeam backup agent' })
+  backupAgentUid?: string;
+
+  @Field({ nullable: true, description: 'Name of a Veeam backup agent job' })
+  name?: string;
+
+  // @Field({ nullable: true })
+  // locationUid?: string;
+
+  // @Field({ nullable: true })
+  // siteUid?: string;
+
+  // @Field({ nullable: true })
+  // organizationUid?: string;
+
+  // @Field({ nullable: true, description: 'Status of the latest job session' })
+  // status:
+  //   | "Unknown"
+  //   | "None"
+  //   | "Idle"
+  //   | "Success"
+  //   | "Warning"
+  //   | "Failed"
+  //   | "Running"
+  //   | "Starting"
+  //   | "Stopping"
+  //   | "Enabling"
+  //   | "Disabling"
+  //   | "WaitingTape"
+  //   | "WaitingRepository";
+
+  // @Field({ nullable: true, description: 'Type of a job configured on a client Veeam Backup & Replication server' })
+  // type?:
+  //   | "Unknown"
+  //   | "BackupVm"
+  //   | "ReplicationVM"
+  //   | "CopyVm"
+  //   | "CopyFile"
+  //   | "FileToTape"
+  //   | "BackupToTape"
+  //   | "BackupCopy"
+  //   | "ImmediateBackupCopy"
+  //   | "SqlLogBackup"
+  //   | "OracleLogBackup"
+  //   | "SureBackup"
+  //   | "AgentPolicy"
+  //   | "AgentBackupJob"
+  //   | "BackupFile"
+  //   | "BackupFileCopy"
+  //   | "AzureBackupJob"
+  //   | "AwsBackupJob"
+  //   | "AhvStorageSnapshotJob";
+
+  // @Field({ nullable: true, description: 'Date and time when the latest job session started' })
+  // lastRun?: string;
+
+  // @Field({ nullable: true, description: 'Date and time when the latest job session ended' })
+  // lastEndTime?: string;
+
+  // @Field({ nullable: true, description: 'Duration of the latest job session, in seconds' })
+  // lastDuration?: number;
+
+  // @Field({ nullable: true, description: 'Rate at which VM data was processed during the latest job session' })
+  // processingRate?: number;
+
+  // @Field({ nullable: true, description: 'Average time a job session takes to complete, in seconds' })
+  // avgDuration?: number;
+
+  // @Field({ nullable: true, description: 'Total amount of data that was transferred to target during the latest job session, in bytes' })
+  // transferredData?: number;
+
+  // @Field({ nullable: true, description: 'Bottleneck in the process of transferring the data from source to target' })
+  // bottleneck?:
+  //   | "Unknown"
+  //   | "Network"
+  //   | "None"
+  //   | "Proxy"
+  //   | "Source"
+  //   | "SourceWanAccelerator"
+  //   | "Target"
+  //   | "TargetWanAccelerator";
+
+  // @Field({ description: 'Indicates whether a job schedule is enabled' })
+  // isEnabled: boolean;
+
+  // @Field({
+  //   nullable: true,
+  //   description: 'Type of a schedule configured for a job'
+  // })
+  // /** Type of a schedule configured for a job. */
+  // scheduleType?:
+  //   | "Unknown"
+  //   | "NotScheduled"
+  //   | "Daily"
+  //   | "Monthly"
+  //   | "Periodically"
+  //   | "Continuously"
+  //   | "BackupWindow"
+  //   | "Chained";
+
+  // @Field({
+  //   nullable: true,
+  //   description: 'Message that is displayed in case a backup job fails, Every line break is represented by the `\r\n` control characters.'
+  // })
+  // failureMessage?: string;
+
+  // @Field({
+  //   nullable: true,
+  //   description: 'Type of a target backup location'
+  // })
+  // targetType?: "Unknown" | "Local" | "Cloud";
+
+  // @Field({
+  //   nullable: true,
+  //   description: 'Name of a target backup location.'
+  // })
+  // destination?: string;
+
+  // @Field({ nullable: true, description: 'Number of retention policy units' })
+  // retentionLimit?: number;
+
+  // @Field({ nullable: true, description: '"Unknown" | "Days" | "RestorePoints"' })
+  // retentionLimitType?: "Unknown" | "Days" | "RestorePoints";
+
+  // @Field({ nullable: true, description: 'Indicates whether the GFS retention is enabled' })
+  // isGfsOptionEnabled?: boolean;
+}
+
+@ObjectType()
 export class VeeamProtectionMeta {
   @Field({ nullable: true })
   backupServerJob?: VeeamProtectionBackupServerJobMeta;
+  @Field({ nullable: true })
+  backupAgentJob?: VeeamProtectionBackupAgentJobMeta;
 }
 
 @ObjectType()
